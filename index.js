1f8c177f7d7659c0ef12d8f60bfae0972c987efc
Senthanal Sirpi Manohar <senthanal@gmail.com>
2021-12-03 11:12:10 +0100

    Added custom route rewrite rules
    Added custom route rewrite rules

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/:resource/:id": "/:resource/:id",
+    "/todo/:id": "/todos/:id",
+    "/todo/": "/todos/"
+}));
 
 server.use(router);
 server.listen(3000, () => {
