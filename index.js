a5cedefa991d8e2b90fbb6d6059804f0615ca319
Senthanal Sirpi Manohar <senthanal@gmail.com>
2021-12-03 19:06:26 +0100

    Added middleware
    Added middleware to configure rewrite urls.

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+server.use(middlewares);
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    "/api/*": "/$1",
-    "/:resource/:id": "/:resource/:id",
-    "/todo/:id": "/todos/:id",
-    "/todo/": "/todos/"
-}));
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/:resource/:id': '/:resource/:id',
+    '/todo/:id': '/todos/:id',
+    '/todo/': '/todos/',
+  })
+);
 
 server.use(router);
 server.listen(3000, () => {
-  console.log('JSON Server is running')
+  console.log('JSON Server is running');
 });
